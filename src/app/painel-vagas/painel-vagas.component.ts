10cdee18b9d4c3d2b1a1be09ec719d48ef9a91e9
TrXGustavoo <gustavofp111@gmail.com>
2022-02-03 17:24:19 -0300

    Finalizando a segunda pagina com os metodos de cadastro, atualizar, e excluir

--- src/app/painel-vagas/painel-vagas.component.ts
+++ src/app/painel-vagas/painel-vagas.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { Vaga } from '../models/Vagas.model';
+import { VagasService } from '../vagas.service';
 
 @Component({
   selector: 'app-painel-vagas',
@@ -7,9 +9,39 @@ import { Component, OnInit } from '@angular/core';
 })
 export class PainelVagasComponent implements OnInit {
 
-  constructor() { }
+  public vaga: Vaga = new Vaga(0,"","","",0);
+
+  constructor(private _vagasService: VagasService) { }
 
   ngOnInit(): void {
   }
 
+  cadastrar(){
+    this._vagasService.cadastrarVagas(this.vaga).subscribe(
+      vaga => {this.vaga = new Vaga(0,"","","",0)},
+      err => {console.log('erro ao cadastrar')}
+    );
+
+    window;location.href = "/mural";
+
+  }
+
+  atualizar(id: number){
+    this._vagasService.atualizarVagas(id,this.vaga).subscribe(
+      vaga => {this.vaga = new Vaga(0,"","","",0)},
+      err => {console.log("Erro ao atualizar")}
+    );
+
+    window.location.href = "/mural";
+
+  }
+
+   excluir(id: number){
+     this._vagasService.removerVagas(id).subscribe(
+       vaga => {this.vaga = new Vaga(0,"","","",0)},
+       err => {console.log("Erro ao excluir")}
+     );
+
+     window.location.href = "/mural";
+   } 
 }
